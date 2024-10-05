2e71f96d60808f8165b5111723aa67f1e4ed1a7a
Anton Waryat <waryataw@gmail.com>
2024-10-05 17:25:32 +0300

    users Api

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,59 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	"github.com/brianvoe/gofakeit"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	desc "github.com/waryataw/auth/pkg/user_v1"
 )
 
+const grpcPort = 50051
+
+type server struct {
+	desc.UnimplementedUserV1Server
+}
+
+// Get ...
+func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("User id: %d", req.GetId())
+
+	roles := []desc.Role{
+		desc.Role_USER,
+		desc.Role_ADMIN,
+	}
+
+	randomIndex := gofakeit.Number(0, len(roles)-1)
+
+	return &desc.GetResponse{
+		Id:        req.GetId(),
+		Name:      gofakeit.Username(),
+		Email:     gofakeit.Email(),
+		Role:      roles[randomIndex],
+		CreatedAt: timestamppb.New(gofakeit.Date()),
+		UpdatedAt: timestamppb.New(gofakeit.Date()),
+	}, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterUserV1Server(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,16 @@
-module github.com/waryataw/microservices_course_boilerplate
+module github.com/waryataw/auth
 
 go 1.23
 
-require github.com/fatih/color v1.17.0
+require (
+	github.com/brianvoe/gofakeit v3.18.0+incompatible
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
+)
 
 require (
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
 )
